fd5d8d28f6affb1310b7fc7658630442b74b729e
Mohamed Tamer <83618615+Mohamedtamer1265@users.noreply.github.com>
2025-09-02 20:00:50 +0300

    fix setup for Vercel deployment

--- server/index.js
+++ server/index.js
@@ -6,7 +6,7 @@ import gymsRouter from "./routes/gyms.js";
 
 dotenv.config();
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 8080 || process.env.PORT;
 
 app.use(cors());
 app.use(express.json());
